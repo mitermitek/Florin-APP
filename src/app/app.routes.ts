f3a97579e09890e9494acb0d820dade13f78e247
mitermitek <188499468+mitermitek@users.noreply.github.com>
2025-11-23 11:34:14 +0100

    feat: add dashboard and error components with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard)
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    'path': '**',
+    loadComponent: () => import('./error/error').then(m => m.Error)
+  }
+];
