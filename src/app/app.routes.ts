6a8dbb0aeaceea4c439158c3890041f89313d859
mitermitek <188499468+mitermitek@users.noreply.github.com>
2026-01-12 23:13:44 +0100

    feat: implement auth and guest guards for route protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,28 @@
 import { Routes } from '@angular/router';
 import { accountResolver } from './accounts/account/account-resolver';
+import { authGuard } from './auth/auth-guard';
+import { guestGuard } from './auth/guest-guard';
 
 export const routes: Routes = [
   {
     path: 'auth/register',
     loadComponent: () => import('./auth/register/register').then((m) => m.Register),
+    canActivate: [guestGuard],
   },
   {
     path: 'auth/login',
     loadComponent: () => import('./auth/login/login').then((m) => m.Login),
+    canActivate: [guestGuard],
   },
   {
     path: 'accounts',
     loadComponent: () => import('./accounts/accounts').then((m) => m.Accounts),
+    canActivate: [authGuard],
   },
   {
     path: 'accounts/:id',
     loadComponent: () => import('./accounts/account/account').then((m) => m.Account),
+    canActivate: [authGuard],
     resolve: {
       account: accountResolver,
     },
@@ -24,5 +30,6 @@ export const routes: Routes = [
   {
     path: 'categories',
     loadComponent: () => import('./categories/categories').then((m) => m.Categories),
+    canActivate: [authGuard],
   },
 ];
